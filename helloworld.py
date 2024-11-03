0f27ef66a4726a004da6f8d275f8744b003bc8e4
Prithwiraj Saha <sahaprithwiraj07@gmail.com>
2024-11-03 11:33:43 +0530

    Loops

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,9 @@
 print("hello world")
+
+# git add .
+# git commit -m "Commit-statement"
+
+#{git pull origin main} 
+
+# git push origin main
+
---
+++ loops.py
@@ -0,0 +1,21 @@
+# using of while loops
+c="hello guys"
+w = 1
+while w <= 5 : 
+ #   print("Heloo")
+    w = w+1 #increases "w" by 1
+
+# another example
+
+i=12
+while i>=1:
+    print(i)
+    i-=1
+    
+print("loop ends here")    
+
+
+
+
+
+
